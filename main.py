1003f697b32960206de02655adf31e8e1bf5c7cb
Vasa Purna Praneeth <128016507+PraneethVasa@users.noreply.github.com>
2023-07-31 00:53:11 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,38 +1,60 @@
-import textbase
+import openai
 from textbase.message import Message
 from textbase import models
 import os
 from typing import List
 
-# Load your OpenAI API key
-models.OpenAI.api_key = "YOUR_API_KEY"
-# or from environment variable:
-# models.OpenAI.api_key = os.getenv("OPENAI_API_KEY")
+# Replace 'YOUR_OPENAI_API_KEY' with your actual OpenAI API key
+openai.api_key = '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'
 
 # Prompt for GPT-3.5 Turbo
 SYSTEM_PROMPT = """You are chatting with an AI. There are no specific prefixes for responses, so you can ask or talk about anything you like. The AI will respond in a natural, conversational manner. Feel free to start the conversation with any question or topic, and let's have a pleasant chat!
 """
 
+def generate_quiz_question():
+    # Function to generate a random quiz question using OpenAI's GPT-3.5
+    response = openai.Completion.create(
+        engine="text-davinci-002",
+        prompt="Generate a trivia question.",
+        max_tokens=100,
+        stop="\n"
+    )
+    question = response['choices'][0]['text'].strip()
+    return question
 
-@textbase.chatbot("talking-bot")
-def on_message(message_history: List[Message], state: dict = None):
-    """Your chatbot logic here
+def evaluate_answer(question, user_answer):
+    # Function to evaluate the user's answer to the quiz question using OpenAI's GPT-3.5
+    response = openai.Completion.create(
+        engine="text-davinci-002",
+        prompt=f"Evaluate the answer to the following question:\nQuestion: {question}\nAnswer: {user_answer}\n",
+        max_tokens=100,
+        stop="\n"
+    )
+    feedback = response['choices'][0]['text'].strip()
+    return feedback
+
+@textbase.chatbot("trivio-bot")
+def on_message(message_history: List[Message]):
+    """TrivioBot chatbot logic
     message_history: List of user messages
-    state: A dictionary to store any stateful information
 
-    Return a string with the bot_response or a tuple of (bot_response: str, new_state: dict)
+    Return a string with the bot_response
     """
 
-    if state is None or "counter" not in state:
-        state = {"counter": 0}
-    else:
-        state["counter"] += 1
+    # Get the latest user message
+    user_message = message_history[-1].text
 
-    # # Generate GPT-3.5 Turbo response
-    bot_response = models.OpenAI.generate(
-        system_prompt=SYSTEM_PROMPT,
-        message_history=message_history,
-        model="gpt-3.5-turbo",
-    )
+    # Check if user wants to exit the quiz
+    if user_message.lower() in ['exit', 'bye', 'quit']:
+        return "TrivioBot: Thanks for playing! Goodbye!"
 
-    return bot_response, state
+    # Generate a random quiz question
+    quiz_question = generate_quiz_question()
+    bot_response = f"TrivioBot: Here's your question: {quiz_question}"
+
+    return bot_response
+
+if __name__ == "__main__":
+    # You can use this part to test the TrivioBot using the textbase UI
+    models.OpenAI.api_key = "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"
+    textbase.run("talking-bot")
